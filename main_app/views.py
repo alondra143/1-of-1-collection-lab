a867c27d229ae2671d4ca46bb5f49885aa78f1c6
alondra 3 <not given>
2021-10-18 19:01:31 -0700

    update views.about, about.html, base.html

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
     return HttpResponse('<h1>Hi testing :)</h1>')
 
 def about(request):
-    return HttpResponse("<h1>What's 1 of 1?</h1>")
+    return render(request, 'about.html')
