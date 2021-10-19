3847ac7130121b2b86ee75cac6f7a1d671fbd392
alondra 3 <not given>
2021-10-18 19:28:17 -0700

    migrate fish model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 
 # Create your models here.
+class Fish(models.Model):
+    """Model definition for Fish."""
+
+    # TODO: Define fields here
+    name = models.CharField(max_length=100)
+    date_added = models.DateTimeField('date posted')
+    age = models.IntegerField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        """Unicode representation of Fish."""
+        pass
