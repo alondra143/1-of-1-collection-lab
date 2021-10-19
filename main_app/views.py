f26c354c41caad0e6eecb4a289c0279111987a05
alondra 3 <not given>
2021-10-18 19:24:04 -0700

    change from collections to fish

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
 # Create your views here.
 
 def home(request):
@@ -8,3 +22,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'fish/index.html', {'cats': cats})
