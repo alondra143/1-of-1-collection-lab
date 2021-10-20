4370be037794e6ea037334359bbc61a08272d055
alondra 3 <not given>
2021-10-19 19:52:08 -0700

    add feeding functionality and form to fish detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('fishes/create/', views.FishCreate.as_view(), name='fishes_create'),
     path('fish/<int:pk>/update/', views.FishUpdate.as_view(), name='fish_update'),
     path('fish/<int:pk>/delete/', views.FishDelete.as_view(), name='fish_delete'),
+    path('fish/<int:fish_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Fish
 from django.http import HttpResponse
+from .forms import FeedingForm
 
 # Create your views here.
 
@@ -16,8 +17,9 @@ def fish_index(request):
     return render(request, 'fish/index.html', {'fishes': fishes})
 
 def fish_detail(request, fish_id):
+    feeding_form = FeedingForm()
     fish = Fish.objects.get(id=fish_id)
-    return render(request, 'fish/detail.html', {'fish': fish})
+    return render(request, 'fish/detail.html', {'fish': fish, 'feeding_form': feeding_form})
 
 class FishCreate(CreateView):
     model = Fish
@@ -26,7 +28,15 @@ class FishCreate(CreateView):
 class FishUpdate(UpdateView):
     model = Fish
     fields = ['age', 'description']
-    
+
 class FishDelete(DeleteView):
     model = Fish
     success_url ='/fishes/'
+
+def add_feeding(request, fish_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit = False)
+        new_feeding.fish_id = fish_id
+        new_feeding.save()
+    return redirect('detail', fish_id=fish_id)
